b39f61cd446b16812e9b6928a18ca5fb03a5b16c
João Batista Gomes Silva <jhonxbatista@gmail.com>
2018-10-01 12:10:44 -0300

    add histogram

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from matplotlib import pyplot as plt
 '''
 	Métodos utilitários
 '''
-def cropt(image, height, width):
-	(h, w) = image.shape
+def cropt(image, height, width, rgb=False):
+	if(rgb):
+		(h, w, c) = image.shape
+	else:
+		(h, w) = image.shape
+
 	return image[0:(h-height), 0:(w-width)]
 
 def negative(image):
@@ -24,31 +28,69 @@ img1ga = cv2.cvtColor(img1a, cv2.COLOR_BGR2GRAY)
 img1g = cropt(img1g, 100, 0)
 img1ga = cropt(img1ga, 100, 0)
 
-# # 3 - visualizar imagem 
-# plt.subplot(121),plt.imshow(img1g, cmap = 'gray')
-# plt.title('Imagem sem ataque quimico'), plt.xticks([]), plt.yticks([])
-
-# plt.subplot(122),plt.imshow(img1ga, cmap = 'gray')
-# plt.title('Imagem com ataque quimico'), plt.xticks([]), plt.yticks([])
-# plt.show()
-
-# # 2 - gerar o histograma da imagem
-# plt.hist(img1g.ravel(), 256, [0, 256])
-# plt.show()
-# plt.hist(img1ga.ravel(), 256, [0, 256])
-# plt.show()
 
 # 3 - determinar o limiar da imagem
 ret, img1gb = cv2.threshold (img1g, 110,255, cv2.THRESH_BINARY)
 ret, img1gab = cv2.threshold (img1ga, 110,255, cv2.THRESH_BINARY)
 
-plt.subplot(1, 2, 1,)
-plt.imshow(negative(img1gb), cmap = 'gray')
-plt.title("Figure 1: normal image")
+fig = plt.gcf()
+fig.canvas.set_window_title('iron nodule images + result')
 
-plt.subplot(1, 2, 2)
+plt.subplot(2, 2, 1)
+plt.imshow(cropt(img1, 100, 0, True))
+plt.title("normal image")
+plt.xticks([])
+plt.yticks([])
+
+plt.subplot(2, 2, 2)
+plt.imshow(cropt(img1a, 100, 0, True))
+plt.title("attack image")
+plt.xticks([])
+plt.yticks([])
+
+plt.subplot(2, 2, 3)
+plt.imshow(negative(img1gb), cmap = 'gray')
+plt.title("normal treshold + invert")
+plt.xticks([])
+plt.yticks([])
+
+plt.subplot(2, 2, 4)
 plt.imshow(negative(img1gab), cmap = 'gray')
-plt.title("Figure 2: attack image")
+plt.title("attack treshold + invert")
+plt.xticks([])
+plt.yticks([])
+
+plt.show()
+
+
+# 3 - gerar o histograma da imagem
+fig = plt.gcf()
+fig.canvas.set_window_title('iron nodule histogram + result')
+
+plt.subplot(2, 2, 1)
+plt.hist(img1g.ravel(), 256, [0, 256])
+plt.title('normal image'), 
+plt.xticks([]), 
+plt.yticks([])
+
+plt.subplot(2, 2, 2)
+plt.hist(img1ga.ravel(), 256, [0, 256])
+plt.title('attack image'), 
+plt.xticks([]), 
+plt.yticks([])
+
+
+plt.subplot(2, 2, 3)
+plt.hist(negative(img1gb).ravel(), 256, [0, 256])
+plt.title('normal treshold + invert'), 
+plt.xticks([]), 
+plt.yticks([])
+
+plt.subplot(2, 2, 4)
+plt.hist(negative(img1gab).ravel(), 256, [0, 256])
+plt.title('attack treshold + invert'), 
+plt.xticks([]), 
+plt.yticks([])
 
 plt.show()
 
